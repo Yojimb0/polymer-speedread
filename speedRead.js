72d80871a2c5427bf96c95edd1ff4758e8c8cd26
Yojimbo <pypointereau+web@gmail.com>
2014-04-07 12:02:45 +1000

    Update component version

---
+++ speedRead.js
@@ -0,0 +1,3 @@
+function SpeedRead($scope){
+	$scope.data = {content: 'Bacon is a meat product prepared from a pig and usually cured. It is first cured using large quantities of salt, either in a brine or in a dry packing; the result is fresh bacon (also known as green bacon). Fresh bacon may then be further dried for weeks or months in cold air, or it may be boiled or smoked.'};
+}
